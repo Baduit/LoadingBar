00d76c453f376b94f3cababb3b6c7c8e468bad4a
Baduit <simon.bertho@epitech.eu>
2020-10-10 14:51:28 +0200

    First commit yeah

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace loading_bar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("[");
+            Console.CursorLeft += 10;
+            Console.Write("]");
+            Console.CursorLeft = 1;
+            for (int i = 0; i < 10; ++i)
+            {
+                Thread.Sleep(500);
+                if (i == 8)
+                    Console.Write('*');
+                else
+                    Console.Write('|');
+            }
+        }
+    }
+}
